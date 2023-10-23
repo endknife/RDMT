b6f564e92f96f50d8a2cb92e37fa38f1e25fb0fe
lsndrcl <144732633+lsndrcl@users.noreply.github.com>
2023-10-23 18:32:58 +0200

    Comments Update master.ino

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -21,11 +21,14 @@ void setup(){
 void loop(){
     const char* msg;  //il warining vuole un const ma noi non glielo diamo... 
 
+    //If the serial port is open and available creates the string that will be sent
     if(Serial.available() > 0){
       messaggio=Serial.readStringUntil('\n');
 
       msg = messaggio.c_str();
-     
+
+      
+      //Sends message 
       Serial.println("send");
       driver.send((uint8_t *)msg, strlen(msg));
       driver.waitPacketSent();
